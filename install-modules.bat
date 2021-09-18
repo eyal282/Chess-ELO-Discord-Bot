11de616e958f6d4c2f0aa6a1f7bd8262415c0286
eyal282 <39803639+eyal282@users.noreply.github.com>
2021-09-18 23:02:42 +0300

    Add files via upload

---
+++ install-modules.bat
@@ -0,0 +1,6 @@
+npm install node.js
+npm install -g --add-python-to-path --vs2015 --production windows-build-tools
+npm install -g node-gyp@latest
+npm install discord.js
+npm install enmap
+npm install node-fetch

e54d05a089cc89e151238e070dbf7f297f98ea61
eyal282 <39803639+eyal282@users.noreply.github.com>
2021-09-18 23:42:56 +0300

    Delete install-modules.bat

--- install-modules.bat
+++
@@ -1,6 +0,0 @@
-npm install node.js
-npm install -g --add-python-to-path --vs2015 --production windows-build-tools
-npm install -g node-gyp@latest
-npm install discord.js
-npm install enmap
-npm install node-fetch

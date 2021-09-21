450f866e85e3b77aa4704f2939d1463ffd12be5a
eyal282 <39803639+eyal282@users.noreply.github.com>
2021-09-21 16:56:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
+
 const Discord = require('discord.js');
 const { MessageEmbed } = require('discord.js');
 const { Permissions } = require('discord.js');
